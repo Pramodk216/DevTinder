383ad7a0c1b062cea3363d3ad653c107e34c065e
pramod.k <pramod.k@zebpay.com>
2026-07-11 17:25:48 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",  (req,res) => {
-    res.send("Hello NodeJs!");
+
+
+// This will handle only GET requests to the /user path
+app.get("/user",  (req,res) => {
+    res.send({name: "John", age: 20});
 })
 
-
-app.use("/hello",  (req,res) => {
-    res.send("Hello World!");
+// This will handle only POST requests to the /user path
+app.post("/user",  (req,res) => {
+    res.send("User created successfully");
 })
 
+// This will handle only DELETE requests to the /user path
+app.delete("/user",  (req,res) => {
+    res.send("User deleted successfully");
+})
+
+// This will handle all HTTP requests(GET, POST, DELETE, etc.) that are made to the /test path
 app.use("/test",  (req,res) => {
     res.send("This is a test page");
+})
+
+app.use("/",  (req,res) => {
+    res.send("Hello NodeJs!");
 })
 
 app.listen(3000, () => {
