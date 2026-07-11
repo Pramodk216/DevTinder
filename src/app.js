5c389c481d69c349f3552512d91091f052bb7517
pramod.k <pramod.k@zebpay.com>
2026-07-11 18:57:51 +0530

    handling multiple route handler separatly

--- src/app.js
+++ src/app.js
@@ -2,27 +2,28 @@ const express = require('express');
 
 const app = express();
 
+app.use("/", (req, res, next) => {
+    console.log("Root Route handler");
+    next();
+})
+
 app.use("/user", (req, res, next) => {
     console.log("First Route handler");
     next();
     // res.send("Response");
-}, [(req, res, next) => {
+})
+
+app.use("/user", (req, res, next) => {
     console.log("Second Route handler");
     next();
-    // res.send("2nd response");
-}, (req, res, next) => {
-    console.log("Third Route handler");
-    next();
-    // res.send("3rd response");
-}], (req, res, next) => {
-    console.log("Fourth Route handler");
-    next();
-    // res.send("4th response");
-}, (req, res, next) => {
-    console.log("Fifth Route handler");
-    res.send("5th Response");
-   
 })
+
+app.use("/user", (req, res, next) => {
+    console.log("Second Route handler");
+    res.send("3rd Response");
+})
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
