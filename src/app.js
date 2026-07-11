dc7c380cef4c757f9a958669accc69fd7df3231c
pramod.k <pramod.k@zebpay.com>
2026-07-11 19:28:24 +0530

    wildcard error handling

--- src/app.js
+++ src/app.js
@@ -1,32 +1,23 @@
 const express = require('express');
-const { authAdmin, authUser } = require('./middlewares/auth');
 
 const app = express();
 
-app.use("/admin", authAdmin);
 
-
-
-app.post("/user/login", (req, res, next)=>{
-    console.log("User login");
-    res.send("User is logged in");
-});
-
-app.get("/user/data", authUser, (req, res, next)=> {
-    console.log("User data");
-    res.send("Get all User Data");
-});
-
-app.get("/admin/getAllData", (req, res, next) => {
-    console.log("getAllData Route handler");
-    res.send("Get all data");
+app.get("/getAllData", (req, res, next) => {
+    try{
+        throw new Error("This is a test error");
+        res.send("Get all data");
+    }catch(err){
+        res.status(500).send("Something went wrong!");
+    }
 })
 
-app.delete("/admin/deleteUser", (req, res, next) => {
-    console.log("deleteUser Route handler");
-    res.send("Delete a user");
-})
 
+app.use("/",(err, req, res, next) => {
+    if(err){
+        res.status(500).send("Something broke!");
+    }
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
