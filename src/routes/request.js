dca6f8989a645e69ee8ab1c8a844f414f436c3ee
pramod.k <pramod.k@zebpay.com>
2026-07-16 22:08:17 +0530

    Logical DB Query & Compound Indexes

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,13 +1,53 @@
 const express = require("express");
 const {authAdmin} = require("../middlewares/auth");
 const User = require("../models/user");
+const ConnectionRequest = require("../models/connectionRequest");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnection", authAdmin, async (req, res) => {
+requestRouter.post("/request/send/:status/:toUserId", authAdmin, async (req, res) => {
 
     try{
-        res.send(req.user.firstName + " has sent you a connection");
+
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+        
+        
+        const validStatus = ["ignored", "interested"];
+
+        if(!(validStatus.includes(status))){
+            return res.status(400).json({ message: `In valid action: ${status}`});
+        }
+
+
+        if(fromUserId === toUserId){
+            return res.status(400).json({ message: "Can't send request to self"});
+        }
+
+        const toUser = await User.findById(toUserId);
+
+        if(!toUser){
+            return res.status(400).json({ message: "Invid user to send Connection requeat"});
+        }
+
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or: [{ fromUserId, toUserId}, {fromUserId: toUserId, toUserId: fromUserId}]
+        })
+
+        if(existingConnectionRequest){
+            return res.status(400).json({ message: "Connection requeat is already present"});
+        }
+
+        const connectionRequest = new ConnectionRequest({fromUserId, toUserId, status});
+
+        const data = await connectionRequest.save();
+
+        res.json({ 
+            message: `${req.user.firstName} is ${status} in ${toUser.firstName}`,
+            data: data
+        });
+
     }catch(err){
         res.status(400).send("Error" + err);
     }
